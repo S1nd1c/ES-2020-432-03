cd03020047dfb053fafe5dc535947589f19f5cbc
Nil Folquer <nfolquer@gmail.com>
2020-05-15 18:14:05 +0200

    Implementada la clase PaymentData

--- PaymentData.py
+++ PaymentData.py
@@ -1,4 +1,8 @@
 class PaymentData:
 
-    def __init__(self):
-        pass
+    def __init__(self, tipus, nom, numero, codi, preu):
+        self.tipus = tipus
+        self.nom = nom
+        self.numero = numero
+        self.codi = codi
+        self.preu = preu
