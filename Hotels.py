232996604fac742d0e78f3412e4f6a7b502a4f12
ikerlopez11 <ikerloma.il@gmail.com>
2020-05-15 19:01:11 +0200

    Afegida la classe Hotels

--- Hotels.py
+++ Hotels.py
@@ -1,4 +1,11 @@
 class Hotels:
 
-    def __init__(self):
-        pass
+    def __init__(self, id_hotel, ciutat, direccio, preu, data_entrada, data_sortida, num_habs):
+        self.id_hotel = id_hotel
+        self.ciutat = ciutat
+        self.direccio = direccio
+        self.preu = preu
+        self.data_entrada = data_entrada
+        self.data_sortida = data_sortida
+        self.num_habs = num_habs
+        
