bf6b22d3ec710b6deb744bc7f71823a2a223cada
Nil Folquer <nfolquer@gmail.com>
2020-05-15 18:16:57 +0200

    Afegida la classe Flights

--- Flights.py
+++ Flights.py
@@ -1,4 +1,6 @@
 class Flights:
 
-    def __init__(self):
-        pass
+    def __init__(self, codi_vol, destinacio, num_passatgers):
+        self.codi_vol = codi_vol
+        self.destinacio = destinacio
+        self.num_passatgers = num_passatgers
