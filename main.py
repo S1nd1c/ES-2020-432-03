18a7f991133e0c72abf95367da215a3dda38bbbf
Elian <elian_fl@hotmail.com>
2020-05-16 18:16:55 +0200

    Listas de Hot,Car,Vol

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from Viaje import Viaje
 from User import User
+from Cars import Cars
 import Bank
 
 # Definicion de todos los vuelos, hoteles y coches
 
+car_1 = 'BMW'
+car_2 = 'Audi'
+car_3 = 'Seat'
+car_4 = 'Renault'
+car_5 = 'Peugeot'
 
 #Listas con vuelos, hoteles y coches
 
 flight_list = []
 hotel_list = []
-car_list = []
+car_list = [car_1,car_2,car_3,car_4,car_5]
 
 
 # Aqui va el flujo principal más los subflujos
