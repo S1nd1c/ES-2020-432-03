d3b5605d81c072f9423508bcda5b631fcd6a59b7
Elian <elian_fl@hotmail.com>
2020-05-16 17:55:04 +0200

    Error corregido

--- Viaje.py
+++ Viaje.py
@@ -5,10 +5,8 @@ from Cars import Cars
 
 class Viaje:
 
-    def __init__(self, user:User):
+    def __init__(self, user:User, llista_vols):
         self.user = user
-
+    
     def sumaPrecios(self, precio_hoteles, precio_coches, precio_vuelos):
-        self.precioTotal =  precio_coches + precio_hoteles + precio_vuelos
-
-    def  s
+        self.precioTotal =  precio_coches + precio_hoteles + precio_vuelos    
