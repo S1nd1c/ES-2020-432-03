40b08cc806d3ff1d5eacfa49c6a2ed7b45cda4dd
Pol Fernández <pol.fernandez.delacruz@gmail.com>
2020-05-16 19:04:05 +0200

    Actualitzacio Payment data

--- PaymentData.py
+++ PaymentData.py
@@ -6,3 +6,9 @@ class PaymentData:
         self.numero = numero
         self.codi = codi
         self.preu = preu
+
+    def solicita_dades_pagament(nom,preu):
+        nb = input('Introdueix el tipus de pagament: ')
+        nb = input('Introdueix el numero de la targeta: ')
+        nb = input('Introdueix el codi de la targeta: ')
+        PaymentData(tipus,nom,numero,codi,preu)
--- User.py
+++ User.py
@@ -4,13 +4,12 @@ from Viaje import Viaje
 
 class User:
 
-    def __init__(self, nom, dni, telefon, direccio, email, pagament:PaymentData):
+    def __init__(self, nom, dni, telefon, direccio, email):
         self.nom = nom
         self.dni = dni
         self.telefon = telefon
         self.direccio = direccio
         self.email = email
-        self.pagament = pagament
 
     def validateInput():
         return type(self.nom) == str and type(self.dni) == str and type(self.telefon) == str and type(self.direccio) == str and tpye(self.email) == str
@@ -18,7 +17,8 @@ class User:
     def pagament(self, viaje:Viaje):
         viaje.sumaPrecios()
         preu = viaje.precioTotal
-        if (Bank.do_payment(self,self.pagament)):
+        self.dades_pagament = PaymentData.solicita_dades_pagament(self.nom,preu)
+        if (Bank.do_payment(self,self.dades_pagament)):
             return True
         else:
             print("Error: Pagament no realitzat correctament")
