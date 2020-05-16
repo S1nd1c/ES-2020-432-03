ac7dac1f197d11d63d0b587c9e6d4eac5ec60ab0
Pol Fernández <pol.fernandez.delacruz@gmail.com>
2020-05-16 19:16:38 +0200

    Afegir preu

--- Cars.py
+++ Cars.py
@@ -3,7 +3,7 @@ import User
 
 class Cars:
 
-    def __init__(self, id_cotxe, tipus, marca, ubi_recollida, ubi_devolucio, dias_estancia):
+    def __init__(self, id_cotxe, tipus, marca, ubi_recollida, ubi_devolucio, dias_estancia,preu):
         self.id_cotxe = id_cotxe
         self.tipus = tipus
         self.marca = marca
@@ -11,6 +11,7 @@ class Cars:
         self.ubi_recollida = ubi_recollida
         self.ubi_devolucio = ubi_devolucio
         self.dias_estancia = dias_estancia
+        self.preu = preu
 
     def reserva_coche(self, user:User):
         rentalcar = Rentalcars.Rentalcars()
