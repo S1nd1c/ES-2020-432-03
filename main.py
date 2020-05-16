74e658964c63b2a394b7068d52363c59d53a1866
Nil Folquer <nfolquer@gmail.com>
2020-05-16 18:08:29 +0200

    Definida estructura en el main

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from Viaje import Viaje
 from User import User
 import Bank
 
+# Definicion de todos los vuelos, hoteles y coches
+
+
+#Listas con vuelos, hoteles y coches
+
+flight_list = []
+hotel_list = []
+car_list = []
+
 
 # Aqui va el flujo principal más los subflujos
 def flux():
