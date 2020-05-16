f02c2eee6828b49de4d097cb5f09030674dbbb06
Nil Folquer <nfolquer@gmail.com>
2020-05-16 17:44:17 +0200

    Primera versio del main i afegida validacio d'input al user

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from Viaje import Viaje
+from User import User
+import Bank
+
+
 # Aqui va el flujo principal más los subflujos
-
-
 def flux():
 
-    pass
+    user = User('Jesus Gil y Gil', '75245896W', '654879524', 'Calle Alamo 23, Marbella', 'jgil@gmail.com')
+
+    
