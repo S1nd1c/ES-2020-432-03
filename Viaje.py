5f4b6b82bb987e46c8e2d337e36c596e174ccf24
Nil Folquer <nfolquer@gmail.com>
2020-05-16 19:37:42 +0200

    Añadidas funciones de calculod e precios y ajuste de variables

--- Viaje.py
+++ Viaje.py
@@ -6,20 +6,46 @@ from Cars import Cars
 class Viaje:
 
 
-    def __init__(self, user:User, num_viajeros, lista_vuelos, lista_hoteles, lista_coches):
+    def __init__(self, user:User, num_viajeros):
         self.user = user
         self.num_viajeros = num_viajeros
-        self.lista_coches = lista_coches
-        self.lista_vuelos = lista_vuelos
-        self.lista_hoteles = lista_hoteles
         
-    def sumaPrecios(self):
-        self.precioTotal =  precio_coches + precio_hoteles + precio_vuelos
+    #TODO borrar las listas incializadas arriba 
 
-    def addDestino(self, vol:Flights):
-        if vol not in llista_vols:
-            self.llista_vols.append(vol)
+    def calculaPrecioCoche(self, lista_coches):
+        final_price = 0
+
+        for coche in lista_coches:
+            final_price += (coche.preu * (coche.preu % self.num_viajeros))
+        
+        return final_price
+
+    def calculaPrecioHoteles(self, lista_hoteles):
+        final_price = 0
+
+        for hotel in lista_hoteles:
+            final_price += (hotel.preu * hotel.num_habs * hotel.dies_estada)
+
+        return final_price
+
+    def sumaPrecios(self):
+        pass
+
+    def addDestino(self, vuelo:Flights):
+        if vuelo not in self.lista_vuelos:
+            self.lista_vuelos.append(vuelo)
     
-    def rmDestino(self, vol:Flights):
-        if vol not in llista_vols:
-            self.llista_vols.remove(vol)
+    def rmDestino(self, vuelo:Flights):
+        if vuelo not in self.lista_vuelos:
+            self.lista_vuelos.remove(vuelo)
+
+    def añadirVuelos(self, lista_vuelos):
+        self.lista_vuelos = lista_vuelos
+
+    
+    def añadirHoteles(self, lista_hoteles):
+        self.lista_hoteles = lista_hoteles
+
+    
+    def añadirCoches(self, lista_coches):
+        self.lista_coches = lista_coches
