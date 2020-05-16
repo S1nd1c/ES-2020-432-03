32560925e43ac3f1d6505e89de27383bba0f7d42
Elian <elian_fl@hotmail.com>
2020-05-16 13:46:52 +0200

    Inicialització Viaje

--- Viaje.py
+++ Viaje.py
@@ -1,7 +1,10 @@
-
+from . import User
+from . import Cars
+from . import Hotels
+from . import Flights
 
 
 class Viaje:
 
-    def __init__(self):
-        pass
+    def __init__(self, user:User):
+        self.user = user
