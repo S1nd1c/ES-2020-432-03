da001ae89c6cfc0b20c1934d9af6a4b97c989b59
Nil Folquer <nfolquer@gmail.com>
2020-05-17 13:31:39 +0200

    Correcció d'errors i borrada una funcio de paymentdata

--- PaymentData.py
+++ PaymentData.py
@@ -5,9 +5,3 @@ class PaymentData:
         self.nom = nom
         self.numero = numero
         self.codi = codi
-
-    def solicita_dades_pagament(DNI):
-        tipus_targeta = input('Introdueix el tipus de pagament: ')
-        numero = input('Introdueix el numero de la targeta: ')
-        codi = input('Introdueix el codi de la targeta: ')
-        PaymentData(tipus_targeta,nom,numero,codi,preu)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,6 @@
 import unittest
 from User import User
 
-
 user1 = User("Jesus Gil y Gil", "75245896W", "654879524", "Calle Alamo 23, Marbella", "jgil@gmail.com")
 user2 = User("Jesus Gil y Gil", "75245896W", 654879524, "Calle Alamo 23, Marbella", "jgil@gmail.com")
 users = [user1, user2]
