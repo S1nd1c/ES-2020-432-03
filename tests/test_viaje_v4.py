aa587b5b3caa7c6b77724b1901c23f8c11c5e651
Nil Folquer <nfolquer@gmail.com>
2020-05-23 18:44:36 +0200

    Implementacio dels test a la versio 4

--- tests/test_viaje_v4.py
+++ tests/test_viaje_v4.py
@@ -7,9 +7,27 @@ from src.PaymentData import PaymentData
 
 usr = User("Jesus Gil y Gil", "75245896W", "654879524",
            "Calle Alamo 23, Marbella", "jgil@gmail.com")
+           usr = User("Jesus Gil y Gil", "75245896W", "654879524",
+           "Calle Alamo 23, Marbella", "jgil@gmail.com")
+car_1 = Cars("1234ABC", "Suv", "BMW", "Calle Falsa 123", "Aeropuerto", 7, 150)
+car_2 = Cars("5089PFE", "Deportivo", "Audi",
+             "Plaza del Pueblo", "Aeropuerto", 4, 70)
+car_3 = Cars("6205MOA", "Familiar", "Renault",
+             "Calle Karl Marx", "Aeropuerto", 2, 25)
+
+hotel_1 = Hotels(1, "Jerez de la Frontera",
+                 "Morenos, 10, 11402 Jerez de la Frontera, España", 26, 7, 1)
+hotel_2 = Hotels(2, "Talavera de la Reina",
+                 "Roma, 1, 45600 Talavera de la Reina, España", 62, 7, 1)
+
+vuelo_1 = Flights("15612F", "MADRID", 55)
+vuelo_2 = Flights("68745A", "ESTAMBUL", 90)
+vuelo_3 = Flights("86452T", "LONDRES", 85)
+
 
 
 class test_viaje_v4(unittest.TestCase):
 
     def test_volverRealizarPago(self):
+        pass
         
